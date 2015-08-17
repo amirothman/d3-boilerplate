cb7415d28df5ee1ebc6f9609ddf83266317169c1
Amir Othman <th3go2@gmail.com>
2015-08-17 12:58:54 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// var svg = d3.select("body").append("svg");
+
+//var purple = svg.append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple")
+
+// var rect = svg.append("rect")
+//   .attr({
+//     width: "100%",
+//     height: "100%",
+//     fill: "#082642"
+//   })
